884ed7ec81682fef5506600737c10e2bc2ad3eb6
Van Duong <van.duong@stickyeyes.com>
2024-07-04 22:32:33 +0100

    feat: created getdayornight util function

---
+++ src/utils/getDayOrNightIcon.ts
@@ -0,0 +1,7 @@
+
+export function getDayOrNightIcon(iconName: string, dateTimeString: string) {
+  const hours = new Date(dateTimeString).getHours()
+  const isDayTime = hours >= 6 && hours < 18
+  
+  return isDayTime ? iconName.replace(/.$/, 'd') : iconName.replace(/.$/, 'n')
+}
