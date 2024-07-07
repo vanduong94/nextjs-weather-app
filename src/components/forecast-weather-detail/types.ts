88e5a8d09f4208946e1a37628f26480cb46d6e25
Van Duong <van.duong@stickyeyes.com>
2024-07-07 21:56:54 +0100

    feat: created forecastWeatherDetails types

---
+++ src/components/forecast-weather-detail/types.ts
@@ -0,0 +1,9 @@
+export interface ForecastWeatherDetailProps {
+  weatherIcon: string;
+  date: string;
+  temp: number;
+  feels_like: number;
+  temp_min: number;
+  temp_max: number;
+  description: string;
+}
