9165cf30387419a12f8417b17c16f0862653e116
Van Duong <van.duong@stickyeyes.com>
2024-07-07 21:56:28 +0100

    chore: added whitespace

--- src/utils/metersToKilometers.ts
+++ src/utils/metersToKilometers.ts
@@ -1,4 +1,4 @@
 export function metersToKilometers(visibilityInMeters: number) {
   const visibilityInKilometers = visibilityInMeters / 1000
-  return `${visibilityInKilometers.toFixed(0)}km`
+  return `${visibilityInKilometers.toFixed(0)} km`
 }
