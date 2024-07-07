e718db8e154c275bc4492db1197494f4192f70a0
Van Duong <van.duong@stickyeyes.com>
2024-07-07 21:57:18 +0100

    feat: updated weatherForecaseDetail

--- src/components/forecast-weather-detail/forecast-weather-detail.tsx
+++ src/components/forecast-weather-detail/forecast-weather-detail.tsx
@@ -1,11 +1,55 @@
 import React from 'react'
+import ForecastWeatherDetailProps from './types'
+
 import Container from '../container'
+import WeatherIcon from '../weather-icon/weather-icon'
+import { convertKelvinToCelsius } from '@/utils/convertKelvinToCelsius'
+import format from 'date-fns/format'
+import { parseISO } from 'date-fns'
+import WeatherDetails from '../weather-details'
 
-type Props = {}
-
-const ForecastWeatherDetail = ({}: Props) => {
+const ForecastWeatherDetail: React.FC<ForecastWeatherDetailProps> = ({
+  weather: [
+    {
+      description,
+      icon
+    }
+  ],
+  dt_txt,
+  main: {
+    feels_like,
+    temp,
+  },
+  city,
+  main,
+  wind,
+  visibility
+}) => {
+  console.log('Foobar');
+  console.log(city);
   return (
-    <Container />
+    <Container className='gap-4'>
+      <section className="flex gap-4 items-center px-4">
+        <div>
+          <WeatherIcon iconName={icon}/>
+          <p>{format(parseISO(dt_txt), "dd.MM")}</p>
+          <p className="text-sm">{format(parseISO(dt_txt), "EEEE")}</p>
+        </div>
+        <div className="flex flex-col px-4">
+          <span className="text-5xl">
+            {convertKelvinToCelsius(temp)}°
+          </span>
+          <p className="text-xs space-x-1">
+            <span>Feels like</span>
+            <span>{convertKelvinToCelsius(feels_like)}°</span>
+          </p>
+          <p className="capitalize">{description}</p>
+        </div>
+      </section>
+      <section className="overflow-x-auto flex justify-between gap-4 px-4 w-full pr-10">
+        <WeatherDetails visibility={visibility} main={main} city={city} wind={wind}/>
+      </section>
+    </Container>
   )
 }
 
--- src/components/forecast-weather-detail/index.ts
+++ src/components/forecast-weather-detail/index.ts
@@ -1,1 +1,2 @@
 export { default } from './forecast-weather-detail'
+export * from './types'
