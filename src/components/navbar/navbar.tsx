08722c291da78ccade5b8de50d3188e75693766d
Van Duong <van.duong@stickyeyes.com>
2024-07-08 22:22:48 +0100

    Update navbar.tsx

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -6,24 +6,20 @@ type Props = {}
 
 const Navbar = (props: Props) => {
   const [city, setCity] = useState("")
-  const [error, setError] = useState("")
-
-  const [suggestions, setSuggestions] = useState<string[]>([])
-  const [showSuggestions, setShowSuggestions] = useState(false)
 
   return (
     <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
       <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
         <div className='flex items-center justify-center gap-2'>
-          <h2 className="text-gray-500 text-3xl">Weather</h2>
+          <h1 className="text-gray-500 text-3xl">Weather</h1>
           <MdWbSunny className='text-3xl mt-1 text-yellow-300'/>
         </div>
         <section className="flex gap-2 items-center">
           <MdMyLocation className='text-2xl text-gray-400 hover:opacity-80 cursor-pointer'/>
           <MdLocationOn className='text-3xl'/>
-          <p className="text-slate-900/80 text-sm">UK</p>
+          <p className="text-slate-900/80 text-sm">{props.location}</p>
           <div>
-            <SearchBox setCityWeather={props.setCityWeather}/>
+            <SearchBox setLocation={props.setLocation}/>
           </div>
         </section>
       </div>
