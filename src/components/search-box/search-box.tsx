c8a1bba0ccecab5d9a1b7d6e809675e36dae8687
Van Duong <van.duong@stickyeyes.com>
2024-07-08 09:25:28 +0100

    feat: added formSubmitHandler function

--- src/components/search-box/search-box.tsx
+++ src/components/search-box/search-box.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoSearch } from "react-icons/io5";
 import { cn } from '@/utils/cn';
 import { Props } from './types';
@@ -6,9 +6,17 @@ import { Props } from './types';
 
 
 const SearchBox = (props: Props) => {
+  const [newCity, setNewCity] = useState('')
+  const {setCityWeather} = props
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    setCityWeather(newCity)
+  }
+
   return (
-    <form onSubmit={props.onSubmit} className={cn("flex relative items-center justify-center h-10", props.className)}>
-      <input value={props.value} onChange={props.onChange} type="text" name="" id="" placeholder='Search location...' className='px-4 py-2 w-[230px] border border-gray-300 rounded-l-md focus:outline-none focus:border-blue-500 h-full'/>
+    <form onSubmit={formSubmitHandler} className={cn("flex relative items-center justify-center h-10", props.className)}>
+      <input value={newCity} onChange={(event) => setNewCity(event.target.value)} type="text" name="" id="" placeholder='Search location...' className='px-4 py-2 w-[230px] border border-gray-300 rounded-l-md focus:outline-none focus:border-blue-500 h-full'/>
       <button className='px-4 py-[9px] bg-blue-500 text-white rounded-r-md focus:outline-none hover:bg-blue-600 h-full'>
         <IoSearch />
       </button>
